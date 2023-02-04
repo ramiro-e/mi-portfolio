31e525565298dc80d153140e6650546669e579e6
ramiro-e <ramiroestevez96@gmail.com>
2023-02-04 20:19:19 +0100

    contacto

--- components/Socials/index.js
+++ components/Socials/index.js
@@ -8,7 +8,7 @@ const Socials = ({ className }) => {
     <div className={`${className} flex flex-wrap mob:flex-nowrap link`}>
       {yourData.socials.map((social, index) => (
         <Button key={index} onClick={() => window.open(social.link)}>
-          {social.title}
+          <img src={social.image} style={{height: "20px"}} className="pr-2"></img><div className="pt-1">{social.title}</div>
         </Button>
       ))}
     </div>
